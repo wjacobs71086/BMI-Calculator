8f9ed430f952627a1235c1b42b929e974027d992
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-06 11:56:08 -0700

    sliders now reading out correctly

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let doubleStr = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(doubleStr) ft"
+    }
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value)) Lbs"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
