a396a1a44e6009b016da4700333fae446af8b669
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-06 13:54:06 -0700

    added second view controller

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  BMI Calculator
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    
     @IBAction func heightSliderChange(_ sender: UISlider) {
-        let doubleStr = String(format: "%.2f", sender.value)
-        heightLabel.text = "\(doubleStr) ft"
+        // the slider will represent Inches and the DISPLAY will show the ft and in
+        let inches = Int(sender.value)
+        let feet = inches / 12
+        let leftover = inches % 12
+        
+        heightLabel.text = "\(feet)' \(leftover)\""
     }
     @IBAction func weightSliderChange(_ sender: UISlider) {
         weightLabel.text = "\(Int(sender.value)) Lbs"
@@ -24,6 +27,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let height = pow(heightSlider.value, 2)
+        let weight = weightSlider.value
+        let BMI = (weight / height) * 703
+        let rounded = String(format: "%.1f", BMI)
+        
+        print(rounded)
+       
+        let secondVC = SecondViewController()
+        // we created a blank value on the secondViewController object and we can simply add that value here
+        secondVC.bmiValue = rounded
+        self.present(secondVC, animated: true, completion: nil)
+        
+    }
+    
 }
 
